70901d657bdea0ca141e44dbf3f582e9dc9d78bc
Onur <onurbelek@outlook.com>
2023-01-10 02:02:20 +0000

    debugged new vercel errors with pathlinks

--- components/Layout.js
+++ components/Layout.js
@@ -1,26 +1,26 @@
-import React from 'react'
-import Navbar from '/Navbar'
-import Footer from './Footer'
-import Head from 'next/head'
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Head from "next/head";
 
-export default function Layout({children, title, description, keywords}){
+export default function Layout({ children, title, description, keywords }) {
   return (
     <>
-        <Head>
-        <title>{title? title + ' - Olympus Finchley': 'Olympus Fish and Chips Finchley'}</title>
+      <Head>
+        <title>
+          {title
+            ? title + " - Olympus Finchley"
+            : "Olympus Fish and Chips Finchley"}
+        </title>
         <meta name="description" content={description} />
         <meta name="keywords" content={keywords} />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
-        <div className="flex min-h-screen flex-col justify-between">
-     
-         <main className="layout">
-            {children}
-         </main>
-    
-        </div>
+      <div className="flex min-h-screen flex-col justify-between">
+        <main className="layout">{children}</main>
+      </div>
       <Footer />
     </>
-  )
+  );
 }
